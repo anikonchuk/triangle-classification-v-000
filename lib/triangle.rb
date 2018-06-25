259bb20c26c08abe611fc238f2fc503d7ead223f
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-25 21:27:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
   
   def valid?
-    if @side_a == 0 || @side_b == 0 || @side_c == 0 || @side_a + @side_b < @side_c || @side_a + @side_c < @side_b || @side_b + @side_c < @side_a
+    if @side_a == 0 || @side_b == 0 || @side_c == 0 || @side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_b + @side_c <= @side_a
       false 
     else
       true 
