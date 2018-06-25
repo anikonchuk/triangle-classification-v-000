d9d1b885ef9d3cffece83bdf37a44b1f49953661
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-25 21:17:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(a,b,c)
+    @side_a = a 
+    @side_b = b 
+    @side_c = c 
+  end
+  
+  
+  
+  
+  class TriangleError < StandardError
+    def message
+      "You must enter a valid triangle"
+    end
+  end
+
 end
