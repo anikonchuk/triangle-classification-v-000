bc2f38b7df3a8c607a66e17c2ef6ea27ba402ae8
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-25 21:26:38 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,9 +18,13 @@ class Triangle
   
   def kind 
     if !valid?
-      begin
+      raise TriangleError
+    elsif @side_a == @side_b && @side_b == @side_c 
+      return :equilateral
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
+      return :isosceles
     else
-      
+      return :scalene
     end
   end
   
