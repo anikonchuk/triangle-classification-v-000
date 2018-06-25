0a542d8f41c9fe3ceb3d2d208e767a06da204373
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-25 21:24:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,21 @@ class Triangle
     @side_c = c 
   end
   
+  def valid?
+    if @side_a == 0 || @side_b == 0 || @side_c == 0 || @side_a + @side_b < @side_c || @side_a + @side_c < @side_b || @side_b + @side_c < @side_a
+      false 
+    else
+      true 
+    end
+  end
   
-  
+  def kind 
+    if !valid?
+      begin
+    else
+      
+    end
+  end
   
   class TriangleError < StandardError
     def message
